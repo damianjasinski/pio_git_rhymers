444c327fb57052033ee0613b283364db617c2462
Szymon Zajac <moniukins@gmail.com>
2021-12-19 20:48:29 +0100

    1.4 stale 1 - Asigned magic values to variables

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -3,8 +3,9 @@ package edu.kis.vh.nursery;
 // alt + ← oraz alt + → zmienia pomiędzy aktualnie otwartymi plikami w naszym edytorze
 //źle sformatowane wiersze to: 5, ,14,15
 public class HanoiRhymer extends DefaultCountingOutRhymer {
+	public static int NONE_REJECTED = 0;
 
-	int totalRejected = 0;
+	int totalRejected = NONE_REJECTED;
 
 	public int reportRejected() {
 		return totalRejected;
