280c8bc4589e942ee681fd90139300a64904fb40
Damian <you@example.com>
2022-01-21 12:47:29 +0100

    1.5.6 Generate Element Comments

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -7,21 +7,40 @@ import edu.kis.vh.nursery.factory.Rhymersfactory;
 
 public class DefaultRhymersFactory implements Rhymersfactory {
 
+	
+	/** 
+	 * @return DefaultCountingOutRhymer
+	 	zwraca obiekt klasy DefaultCountingOutRhymer
+	 */
 	@Override
 	public DefaultCountingOutRhymer getStandardRhymer() {
 		return new DefaultCountingOutRhymer();
 	}
 
+	
+	/** 
+	 * @return DefaultCountingOutRhymer
+	 * zwraca obiekt klasy DefaultCountingOutRhymer
+	 */
 	@Override
 	public DefaultCountingOutRhymer getFalseRhymer() {
 		return new DefaultCountingOutRhymer();
 	}
 
+	
+	/** 
+	 * @return DefaultCountingOutRhymer
+	 * zwraca obiekt klasy DefaultCountingOutRhymer
+	 */
 	@Override
 	public DefaultCountingOutRhymer getFIFORhymer() {
 		return new FIFORhymer();
 	}
 
+	
+	/** 
+	 * @return DefaultCountingOutRhymer
+	 */
 	@Override
 	public DefaultCountingOutRhymer getHanoiRhymer() {
 		return new HanoiRhymer();
